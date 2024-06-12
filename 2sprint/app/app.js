6d5443b824c3ce7214940340c3318d45431fb061
Murvat <murvat.mustafayev@mail.ru>
2024-06-12 19:56:34 +0100

    6sprint

--- 2sprint/app/app.js
+++ 2sprint/app/app.js
@@ -1,28 +1,38 @@
-const transactions = ['571a319c8c', 'bd32bd6c05', '1c28a0c411', '9b425707b3', '979d1c10a1', '94641b2966'];
-const test = [44, 55, 66, 77, 88, 2, 3, 4];
+// const transactions = ['571a319c8c', 'bd32bd6c05', '1c28a0c411', '9b425707b3', '979d1c10a1', '94641b2966'];
+// const test = [44, 55, 66, 77, 88, 2, 3, 4];
 
-// true
-// false
-const str = 'abcdefgh';
+// // true
+// // false
+// const str = 'abcdefgh';
 
-if (str.includes('f1g')) {
-    console.log('yes');
-    // 
+// if (str.includes('f1g')) {
+//     console.log('yes');
+//     //
+// }
+// else {
+//     console.log('no');
+// }
+
+
+// const users = [
+//     { "name": "ivanov", "age": 44 },
+//     { "name": "petrov", "age": 14 },
+//     { "name": "pitrov", "age": 37 },
+//     { "name": "alexeev", "age": 43 },
+// ]
+
+// let newUsers = users.filter(item => {
+//     return item.name.includes('pe')
+// });
+
+// console.log(newUsers);
+function bmi(weight, height) {
+    let bmiElem = weight / height ** 2;
+    let outPut = ''
+    switch (outPut) {
+        case bmiElem > 20:
+            console.log(bmiElem);
+            break
+    }
 }
-else {
-    console.log('no');
-}
-
-
-const users = [
-    { "name": "ivanov", "age": 44 },
-    { "name": "petrov", "age": 14 },
-    { "name": "pitrov", "age": 37 },
-    { "name": "alexeev", "age": 43 },
-]
-
-let newUsers = users.filter(item => {
-    return item.name.includes('pe')
-});
-
-console.log(newUsers);
+bmi(80, 1.80)
